66eea26631b66149b86312257c90099c117a7d7c
hellodword <46193371+hellodword@users.noreply.github.com>
2023-04-28 20:56:02 +0800

    init

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -e
+set -x
+
+curl -f -o ~/.git-completion.bash \
+    https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+cat << EOF >> ~/.bashrc
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi
+EOF
+
+yarn install
