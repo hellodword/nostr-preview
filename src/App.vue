18b4d25941337794bc928787be777f520777bfeb
hellodword <46193371+hellodword@users.noreply.github.com>
2023-04-30 22:02:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,8 @@ import {
 import hljs from 'highlight.js'
 import 'highlight.js/styles/github.css'
 import MarkdownIt from 'markdown-it'
+import mathjax3 from 'markdown-it-mathjax3'
+import MarkdownItEmoji from 'markdown-it-emoji'
 
 const md = new MarkdownIt({
   html: true,
@@ -31,11 +33,14 @@ const md = new MarkdownIt({
   typographer: true,
   breaks: true,
   highlight(code, lang) {
+    console.log(lang, code);
     // https://github.com/WongSaang/chatgpt-ui/blob/73536144726eb1e3cb37a72cc7861da02b1304cc/components/MsgContent.vue
     const language = hljs.getLanguage(lang) ? lang : 'plaintext'
-    return `<pre class="hljs-code-container my-3"><div class="hljs-code-header d-flex align-center justify-space-between bg-grey-darken-3 pa-1"><span class="pl-2 text-caption">${language}</span><button class="hljs-copy-button" data-copied="false">Copy</button></div><code class="hljs language-${language}">${hljs.highlight(code, { language: language, ignoreIllegals: true }).value}</code></pre>`
+    return `<pre class="hljs-code-container"><code class="hljs language-${language}">${hljs.highlight(code, { language: language, ignoreIllegals: true }).value}</code></pre>`
   },
-});
+})
+  .use(mathjax3)
+  .use(MarkdownItEmoji);
 
 
 export default defineComponent({
@@ -131,22 +136,42 @@ export default defineComponent({
 
 .md-content {
   margin: 8px;
-  font-size: 0.875rem !important;
-  font-weight: 400;
+  /* font-size: 0.875rem !important; */
+  /* font-weight: 400; */
   line-height: 1.25rem;
 }
 
 .md-content p,
 .md-content table,
 .md-content ul,
-.md-content ol,
-.md-content h1,
-.md-content h2,
-.md-content h3,
-.md-content h4,
-.md-content h5,
-.md-content h6 {
+.md-content ol {
   margin-bottom: 1rem;
+}
+
+.md-content h1,
+.md-content hr {
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+
+.md-content h2 {
+  margin-top: 7px;
+  margin-bottom: 7px;
+}
+
+.md-content h3 {
+  margin-top: 6px;
+  margin-bottom: 6px;
+}
+
+.md-content h4 {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.md-content h3 {
+  margin-top: 4px;
+  margin-bottom: 4px;
 }
 
 .md-content table,
@@ -167,36 +192,24 @@ export default defineComponent({
   padding-left: 2em;
 }
 
-.hljs-code-container {
-  border-radius: 3px;
-  overflow: hidden;
+.md-content p>code {
+  padding: 2px 4px;
+  font-size: 90%;
+  color: #c7254e;
+  background-color: #f9f2f4;
+  border-radius: 4px;
 }
 
-.hljs-copy-button {
-  width: 2rem;
-  height: 2rem;
-  text-indent: -9999px;
-  color: #fff;
-  border-radius: .25rem;
-  border: 1px solid #ffffff22;
-  background-image: url('data:image/svg+xml;utf-8,<svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6 5C5.73478 5 5.48043 5.10536 5.29289 5.29289C5.10536 5.48043 5 5.73478 5 6V20C5 20.2652 5.10536 20.5196 5.29289 20.7071C5.48043 20.8946 5.73478 21 6 21H18C18.2652 21 18.5196 20.8946 18.7071 20.7071C18.8946 20.5196 19 20.2652 19 20V6C19 5.73478 18.8946 5.48043 18.7071 5.29289C18.5196 5.10536 18.2652 5 18 5H16C15.4477 5 15 4.55228 15 4C15 3.44772 15.4477 3 16 3H18C18.7956 3 19.5587 3.31607 20.1213 3.87868C20.6839 4.44129 21 5.20435 21 6V20C21 20.7957 20.6839 21.5587 20.1213 22.1213C19.5587 22.6839 18.7957 23 18 23H6C5.20435 23 4.44129 22.6839 3.87868 22.1213C3.31607 21.5587 3 20.7957 3 20V6C3 5.20435 3.31607 4.44129 3.87868 3.87868C4.44129 3.31607 5.20435 3 6 3H8C8.55228 3 9 3.44772 9 4C9 4.55228 8.55228 5 8 5H6Z" fill="white"/><path fill-rule="evenodd" clip-rule="evenodd" d="M7 3C7 1.89543 7.89543 1 9 1H15C16.1046 1 17 1.89543 17 3V5C17 6.10457 16.1046 7 15 7H9C7.89543 7 7 6.10457 7 5V3ZM15 3H9V5H15V3Z" fill="white"/></svg>');
-  background-repeat: no-repeat;
-  background-position: center;
-  transition: background-color 200ms ease, transform 200ms ease-out
+.md-content pre {
+  margin-bottom: 10px;
 }
 
-.hljs-copy-button:hover {
-  border-color: #ffffff44
+.md-content pre>code {
+  background-color: #e6e6e6;
 }
 
-.hljs-copy-button:active {
-  border-color: #ffffff66
-}
-
-.hljs-copy-button[data-copied="true"] {
-  text-indent: 0;
-  width: auto;
-  background-image: none
+.hljs {
+  background-color: #e6e6e6;
 }
 </style>
 <style rel="stylesheet">
@@ -238,7 +251,10 @@ q:after,
 blockquote:before,
 blockquote:after {
   content: "";
-}</style>
-<style>.v-progress-circular {
+}
+</style>
+<style>
+.v-progress-circular {
   margin: 1rem;
-}</style>
+}
+</style>
